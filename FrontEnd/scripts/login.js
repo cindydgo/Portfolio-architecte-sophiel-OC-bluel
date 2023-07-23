e1f9d9224b31ba3a150b359281d736c99b3dd0f9
Cindy Dgo <cindazur99@gmail.com>
2023-07-23 17:33:19 +0200

    Login page integration
    Form page design
    User authentication

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,41 @@
+const loginForm = document.getElementById('form-login')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+
+loginForm.addEventListener("submit", submitForm)
+
+// submit user object in JSON 
+async function submitForm(e) {
+    e.preventDefault();
+    let user = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(user)
+    try {
+        const url = 'http://localhost:5678/api/users/login'
+        const res = await fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user)
+        })
+        console.log(res)
+// SERVER STATUS RESPONSE OK
+        if (res.status === 200) {
+            const data = await res.json();
+            const token = data.token;
+        // save token(jeton d authentification) in local storage
+            localStorage.setItem("token", token);
+            window.location.href ="./index.html" 
+// ERROR SERVER STATUS RESPONSE 
+        } else if (res.status === 401) {
+            alert("E-mail ou mot de passe incorrect")
+        } else if (res.status === 404) {
+            alert("Utilisateur non trouvé");
+        }
+
+// CATCH ERRORS
+    } catch (error) {
+        alert ('Une erreur est survenue lors de la connexion')
+    }
+}
